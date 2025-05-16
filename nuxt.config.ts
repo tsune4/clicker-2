6027fa739f86f414a95adb3f1e781c82739e8cd6
Sotaro Tsuneoka <tunenen305@gmail.com>
2025-05-16 12:45:18 +0900

    feat: アップデート

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss']
+  modules: ['@nuxtjs/tailwindcss'],
+  app: {
+    baseURL: '/', // ローカル生成時は '/' に
+  },
+  nitro: {
+    preset: 'static',
+    prerender: {
+      routes: ['/'] // ここを追加
+    }
+  }
 })
